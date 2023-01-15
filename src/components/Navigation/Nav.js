61ce74fc4fbd79578340c3cd6cb6ec2ad07f2d78
William Barents <w.barents@youweagency.com>
2023-01-15 17:59:03 +0200

    upload user image

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./Nav.module.css";
 
 const Nav = () => {
+  const [showHandle, setShowHandle] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShowHandle(true);
+      } else {
+        setShowHandle(false);
+      }
+      return () => {
+        window.removeEventListener("scroll");
+      };
+    });
+  }, []);
+
   return (
-    <div className={classes["nav"]}>
+    <div className={`${classes["nav"]} ${showHandle && classes["nav__black"]}`}>
       <img
         className={classes["nav__logo"]}
         alt="Netflix Logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1597px-Netflix_2015_logo.svg.png?20190206123158"
+      />
+      <img
+        className={classes["nav__avatar"]}
+        alt="Netflix Avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
       />
     </div>
   );
--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
+import ReactPlayer from "react-youtube";
 import axios from "../../axios";
 import classes from "./Row.module.css";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 const Row = (props) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,6 +19,29 @@ const Row = (props) => {
     fetchData();
   }, [props.fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handlerClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          //https://www.youtube.com/watch?v=0cPCMIuDk2I&ab_channel=EdRoh
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
   return (
     <div className={classes["row"]}>
       <h2>{props.title}</h2>
@@ -23,6 +50,7 @@ const Row = (props) => {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handlerClick(movie)}
             className={`${classes["row__poster"]} ${
               props.isLargeRow && classes["row__posterLarge"]
             }`}
@@ -34,7 +62,7 @@ const Row = (props) => {
         ))}
       </div>
 
-      {/* {Container -> posters} */}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
